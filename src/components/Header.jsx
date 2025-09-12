956500224095e7379686c19c060fa3b72937030f
shaneallmighty <shaneokallahan@hotmail.com>
2025-09-12 16:33:00 +0200

    Hamburger header menu for small screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import logo from "../assets/images/logo.png";
 import "../assets/styles/header.css";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
+      <button className="hamburger" onClick={toggleMenu}>
+        &#9776; {/* Hamburger icon */}
+      </button>
       <div>
         <img src={logo} alt="LogoHeader" className="logo" />
       </div>
       <nav>
-        <ul>
+        <ul className={menuOpen ? "show" : ""}>
           <li>
             <Link to="/">Home</Link>
           </li>
