d71543784f5027a69f227b0cd3dbf2646dfa5d7d
shaneallmighty <shaneokallahan@hotmail.com>
2026-07-01 14:32:34 +0200

    Vercel tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,5 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
